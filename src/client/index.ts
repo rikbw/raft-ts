d44ec7e3e94fadd2b0645df29a50a56943dfb38b
Rik Bauwens <rikbauwens1@gmail.com>
2022-10-17 19:13:09 +0200

    feat: add basic key value server

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,85 @@
-
 import split2 from 'split2'
+import fetch from 'node-fetch'
 
 process.stdin.pipe(split2()).on('data', handleInput)
 
-function handleInput(input: string) {
-    console.log('you sent: ', input)
+const unreachable = (_: never) => { throw new Error('unreachable') }
+
+// TODO this should be in env vars
+const getUrl = ({ path }: {path: string}) => `http://localhost:3000/${path}`
+
+// TODO use shared io-ts types for client-server interactions
+
+async function request({path, method, body }: { path: string; method: string; body?: any }) {
+    const url = getUrl({ path })
+    const result = await fetch(url, { method, body, headers: { 'Content-Type': 'text/plain' } })
+    return result.text();
+}
+
+async function handleInput(input: string) {
+    const command = parseInput(input)
+
+    switch (command.type) {
+        case "get": {
+            const result = await request({ path: `get/${command.key}`, method: 'get'})
+            console.log(result)
+            return;
+        }
+
+        case "delete": {
+            const result = await request({ path: `delete/${command.key}`, method: 'get'})
+            console.log(result)
+            return;
+        }
+
+        case "set": {
+            const { key, value } = command;
+            const result = await request({path: `set/${key}`, method: 'post', body: JSON.stringify(value)})
+            console.log(result)
+            return;
+        }
+
+
+        default:
+            unreachable(command)
+    }
+}
+
+type Command = {
+    type: 'get' | 'delete';
+    key: string
+} | {
+    type: 'set';
+    key: string;
+    value: string;
+}
+
+function parseInput(input: string): Command {
+    // TODO use io-ts
+    const parts = input.split(' ')
+    const command = parts[0]
+
+    switch (command) {
+        case 'get':
+            return {
+                type: 'get',
+                key: parts[1]!
+            }
+
+        case 'set':
+            return {
+                type: 'set',
+                key: parts[1]!,
+                value: parts[2]!
+            }
+
+        case 'delete':
+            return {
+                type: 'delete',
+                key: parts[1]!
+            }
+
+        default:
+            throw new Error(`failed to parse: ${input}`)
+    }
 }
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+
+import Koa from 'koa'
+import Router from '@koa/router'
+import koaBodyParser from 'koa-body'
+
+const app = new Koa();
+
+app.use(koaBodyParser())
+
+const mainRouter = new Router();
+
+const store = new Map<string, string>();
+
+// TODO use shared io-ts types for request bodies and responses
+
+mainRouter.get('/get/:key', (context) => {
+    const { key } = context.params;
+    console.log('got get request', { key })
+    context.body = store.get(key as string)
+})
+
+mainRouter.get('/delete/:key', (context) => {
+    const { key } = context.params;
+    console.log('got delete request', { key })
+    store.delete(key as string)
+    context.body = 'ok'
+})
+
+mainRouter.post('/set/:key', (context) => {
+    const { key } = context.params;
+    const value = context.request.body;
+    console.log('got set request', { key, value})
+    store.set(key as string, value)
+    context.body = 'ok'
+})
+
+app.use(mainRouter.routes())
+
+app.listen(3000)
+
+console.log('listening on port 3000')
